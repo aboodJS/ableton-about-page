f76e22e2d4a7c8f417ce895de8ca5fe2e02968c9
ajs <140010746+aboodJS@users.noreply.github.com>
2026-02-18 21:45:36 +0300

    added a banner section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>home page</h1>
+  <main>
+    <section class="relative h-[130vh]">
+      <div class="absolute grid bg-no-repeat justify-center content-center bg-cover bg-center w-7/12 h-3/4" style="background-image: url('https://images.pexels.com/photos/164745/pexels-photo-164745.jpeg');"><p>Live 12.4 is coming – with Link Audio, updated devices and more</p></div>
+      <div class="absolute grid bg-no-repeat justify-center content-center bg-cover bg-center w-2/6 h-2/4 top-1/4 left-2/3" style="background-image: url('https://images.pexels.com/photos/70911/pexels-photo-70911.jpeg');"><p>Explore Live 12.4’s Updates with Seed To Stage</p></div>
+      <div class="absolute grid bg-no-repeat justify-center content-center bg-cover bg-center w-4/12 h-2/6 top-2/3 left-2/4" style="background-image: url('https://images.pexels.com/photos/2426085/pexels-photo-2426085.jpeg');"><p>A Step-by-Step Guide on Harmonizing and Layering Vocals</p></div>
+    </section>
+  </main>
 </template>
