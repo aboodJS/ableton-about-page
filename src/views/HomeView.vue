19cc8599bf6e7194d23f893e73eb1513a2bf0cb9
ajs <140010746+aboodJS@users.noreply.github.com>
2026-02-19 16:53:01 +0300

    changes to styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,48 @@
 <template>
   <main>
     <section class="relative h-[130vh]">
-      <div class="absolute grid bg-no-repeat justify-center content-center bg-cover bg-center w-7/12 h-3/4" style="background-image: url('https://images.pexels.com/photos/164745/pexels-photo-164745.jpeg');"><p>Live 12.4 is coming – with Link Audio, updated devices and more</p></div>
-      <div class="absolute grid bg-no-repeat justify-center content-center bg-cover bg-center w-2/6 h-2/4 top-1/4 left-2/3" style="background-image: url('https://images.pexels.com/photos/70911/pexels-photo-70911.jpeg');"><p>Explore Live 12.4’s Updates with Seed To Stage</p></div>
-      <div class="absolute grid bg-no-repeat justify-center content-center bg-cover bg-center w-4/12 h-2/6 top-2/3 left-2/4" style="background-image: url('https://images.pexels.com/photos/2426085/pexels-photo-2426085.jpeg');"><p>A Step-by-Step Guide on Harmonizing and Layering Vocals</p></div>
+      <div
+        class="absolute grid bg-no-repeat justify-center bg-cover bg-center w-7/12 h-3/4"
+        style="
+          background-image: url('https://images.pexels.com/photos/164745/pexels-photo-164745.jpeg');
+        "
+      >
+        <p class="text-4xl self-start m-10 text-white font-extrabold">
+          Live 12.4 is coming – with Link Audio, updated devices and more
+        </p>
+      </div>
+      <div
+        class="absolute grid bg-no-repeat justify-center bg-cover bg-center w-2/6 h-2/4 top-1/4 left-2/3"
+        style="
+          background-image: url('https://images.pexels.com/photos/70911/pexels-photo-70911.jpeg');
+        "
+      >
+        <p class="text-2xl self-start m-10 text-white font-extrabold">
+          Explore Live 12.4’s Updates with Seed To Stage
+        </p>
+      </div>
+      <div
+        class="absolute grid bg-no-repeat justify-center bg-cover bg-center w-4/12 h-2/6 top-2/3 left-2/4"
+        style="
+          background-image: url('https://images.pexels.com/photos/2426085/pexels-photo-2426085.jpeg');
+        "
+      >
+        <p class="text-xl self-start m-10 text-amber-400 font-extrabold">
+          A Step-by-Step Guide on Harmonizing and Layering Vocals
+        </p>
+      </div>
+    </section>
+    <section class="flex items-center justify-evenly m-12">
+      <h3 class="text-3xl font-bold">The latest from Ableton</h3>
+      <ul class="flex p-0 m-0 justify-evenly w-2/3 text-xl text-red-600 font-bold">
+        <li>All posts</li>
+        <li>Artists</li>
+        <li>News</li>
+        <li>Downloads</li>
+        <li>Tutorials</li>
+        <li>Videos</li>
+        <li>Loop</li>
+      </ul>
     </section>
   </main>
 </template>
